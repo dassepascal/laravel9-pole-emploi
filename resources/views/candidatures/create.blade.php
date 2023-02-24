2096634e3194e99edaec2f96815be7aea27d7228
dassepascal <pascaldasse56@gmail.com>
2023-02-24 14:15:02 +0100

    rename choose a source with choose a advancement

--- resources/views/candidatures/create.blade.php
+++ resources/views/candidatures/create.blade.php
@@ -59,7 +59,7 @@
                 an option</label>
             <select name="advancement_id"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option selected>Choose a source</option>
+                <option selected>Choose a advancement</option>
                 @foreach ($advancements as $advancement )
                 <option value="{{ $advancement->id }}">{{ $advancement->label_advancement }}</option>
                 @endforeach
